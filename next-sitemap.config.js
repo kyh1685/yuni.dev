64875592550f7c112b6654beae1ab6079aea77ad
KimYunhee <qnvnfls@naver.com>
2023-06-01 22:42:10 +0900

    feat: sitemap 적용

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: "https://yuni-dev.vercel.app",
+  changefreq: "daily",
+  priority: 1,
+  sitemapSize: 7000,
+  generateRobotsTxt: true,
+  exclude: [],
+};
