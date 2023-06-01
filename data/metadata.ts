2b9ee680e56b2d0e2f158986f3aafeff3f64b8fd
KimYunhee <qnvnfls@naver.com>
2023-06-01 15:49:40 +0900

    feat: Open Graph 적용

--- data/metadata.ts
+++ data/metadata.ts
@@ -1,5 +1,10 @@
-const metadata = {
-  title: "Yuni | Frontend Developer",
+export const contact = {
+  email: "yooni97311@gmail.com",
+  github: "https://github.com/kyh1685",
 };
 
-export default metadata;
+export const meta = {
+  url: "https://yuni-dev.vercel.app",
+  title: "Yuni | Frontend Developer",
+  description: "배운 것을 기억하기 위해 기록합니다.",
+};
