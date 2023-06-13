71c579d48466283f45ee2f74638e7a0d3351ade3
KimYunhee <qnvnfls@naver.com>
2023-06-14 00:19:44 +0900

    fix: 다크 모드 새로고침 시 화면 깜빡임 해결

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,11 @@
 import Image from "next/image";
-import { useEffect } from "react";
 
 import Nav from "./Nav";
 
 const Header = () => {
-  useEffect(() => {
-    window.localStorage.getItem("theme") ?? window.localStorage.setItem("theme", "light");
-
-    if (window.localStorage.getItem("theme") === "dark") document.body.className = "dark";
-  }, []);
-
   const themeModeHandle = () => {
-    window.localStorage.setItem("theme", window.localStorage.getItem("theme") === "light" ? "dark" : "light");
-    document.body.classList.toggle("dark");
+    localStorage.theme = localStorage.theme === "light" ? "dark" : "light";
+    document.documentElement.classList.toggle("dark");
   };
 
   return (
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,19 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  const setThemeMode = `
+        if(localStorage.theme === "dark"){
+          document.documentElement.className = "dark";
+        }else{
+          localStorage.theme = "light";
+        }
+      `;
+
   return (
     <Html lang="en">
       <Head>
         <meta name="google-site-verification" content="hW6cK1FvEMGBpQikcfiFQZEnL8hMG-GibEQZC9hjl38" />
+        <script dangerouslySetInnerHTML={{ __html: setThemeMode }} />
       </Head>
       <body>
         <Main />
