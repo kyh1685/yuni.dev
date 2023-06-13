d835607942e252869778bb2b512dccaa3ca44dbc
KimYunhee <qnvnfls@naver.com>
2023-06-14 00:34:31 +0900

    style: 검색바 밑 간격 조절

--- components/Search.tsx
+++ components/Search.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const Search = ({ onChange }: Props) => {
   return (
-    <div className="flex p-5 max-sm:p-3 mb-7 border-[#16181a] dark:border-[#fafafa] border-2 rounded-lg">
+    <div className="flex p-5 max-sm:p-3 mb-16 border-[#16181a] dark:border-[#fafafa] border-2 rounded-lg">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 513.749 513.749"
